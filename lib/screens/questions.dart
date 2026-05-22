9b415c4e8fb3ef562fefe110dfa3821e25058c45
Md Azharul Hoque <mdazharulhoque7@gmail.com>
2026-05-22 15:14:31 +0600

    create answer question method which will invoke on tap answer and navigation to next question

--- lib/screens/questions.dart
+++ lib/screens/questions.dart
@@ -11,9 +11,27 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
-  final QuizQuestion currentQuestion = questions[0];
+  var currentQuestionIndex = 0;
+  
+  void answerQuestion(String selectedAnswer) {
+    // Handle the selected answer (e.g., check if it's correct, update score, etc.)
+    // For now, we'll just print the selected answer to the console.
+    print('Selected answer: $selectedAnswer');
+
+    // Move to the next question
+    setState(() {
+      if (currentQuestionIndex < questions.length) {
+        currentQuestionIndex++;
+      } else {
+        // Handle the end of the quiz (e.g., show results, reset quiz, etc.)
+        print('Quiz completed!');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    late QuizQuestion currentQuestion = questions[currentQuestionIndex];
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -22,20 +40,18 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(currentQuestion.question,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.center,
+            Text(
+              currentQuestion.question,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
             ...currentQuestion.getShuffledAnswers().map((answer) {
-              return AnswerButton(
-                answerText: answer,
-                onTap: () {},
-              );
+              return AnswerButton(answerText: answer, onTap: () => answerQuestion(answer));
             }),
           ],
         ),
