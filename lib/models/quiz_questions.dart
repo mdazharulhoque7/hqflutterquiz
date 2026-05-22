c102b3bbe8503f06477a558a227b4c7b387033f6
Md Azharul Hoque <mdazharulhoque7@gmail.com>
2026-05-22 14:32:12 +0600

    create class method to copy the orginal answer list and shuffle the value to be render on the question screen

--- lib/models/quiz_questions.dart
+++ lib/models/quiz_questions.dart
@@ -3,4 +3,15 @@ class QuizQuestion {
   final List<String> answers;
 
   const QuizQuestion(this.question, this.answers);
+
+  List<String> getShuffledAnswers() {
+    /* 
+    Create a copy of the answers list and shuffle it to avoid modifying the original list.
+    This ensures that the original order of answers is preserved for each question, while still providing a randomized order for display. 
+    */
+
+    final shuffledAnswers = List<String>.from(answers);
+    shuffledAnswers.shuffle();
+    return shuffledAnswers;
+  }
 }
